07aa06ab111e0c2f73bf1fb2d8f10347cbad4bff
JeremieKaszub <jeremie.kaszub@gmail.com>
2013-12-03 20:03:03 +0100

    Commit du fin de journéedu 3 décembre 2013

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+	def logo
+		@logo = image_tag("logo.png", :alt => "Application exemple", :class => "round")
+	end
+
 	def titre
 		base_titre = "Simple App du Tutoriel Ruby on Rails"
 		if @titre.nil?
